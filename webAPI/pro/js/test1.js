641ccbd4da965f2d852f790aae02c551326d2fcc
oyyxmmd <645076602@qq.com>
2019-07-16 18:29:11 +0800

    更新了webAPI部分

---
+++ webAPI/pro/js/test1.js
@@ -0,0 +1,24 @@
+var btn1 = document.getElementById('btn');
+
+// btn1.addEventListener('click', function () {
+//     alert('i am second Event!');
+// });
+btn1.onclick = function () {
+    alert('i am onclick Event!');
+};
+btn1.addEventListener('click', function () {
+    alert('i am first Event!');
+});
+var btn2 = document.getElementById('btn2');
+
+// 测试网页点击文字
+var arr = ['爱过','富强','民主','和谐'];
+var i = 0;
+var body1 = document.body;
+body1.addEventListener('click',function(){
+   console.log(arr[i]);
+    i++;
+    if(i===arr.length){
+        i = 0;
+    }
+});
